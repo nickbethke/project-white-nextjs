7ced317c41cb2f01e199f8803ebf54f057a58e79
nickbethke <nickbethke5599@gmail.com>
2023-10-07 00:14:40 +0200

    Update: file routes

--- app/api/files/type/[type]/route.ts
+++ app/api/files/type/[type]/route.ts
@@ -1,75 +1,82 @@
-import {NextRequest, NextResponse} from "next/server";
-import {prismaDB} from "@/lib/prisma";
-import {FileType} from "@/lib/files";
+import { NextRequest, NextResponse } from "next/server";
+import { prismaDB } from "@/lib/prisma";
+import { FileType } from "@/lib/files";
 import {
-    allowedAudioTypes,
-    allowedDocumentTypes,
-    allowedImageTypes,
-    allowedTypes,
-    allowedVideoTypes
+  allowedAudioTypes,
+  allowedDocumentTypes,
+  allowedImageTypes,
+  allowedTypes,
+  allowedVideoTypes,
 } from "@/lib/constants/filetypes.constants";
-import {getResponse} from "@/lib/utils";
-import {forEach} from "lodash";
+import { getResponse } from "@/lib/utils";
+import { forEach } from "lodash";
 
-export async function GET(req: NextRequest, {params}: { params: { type: string } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { type: string } }
+) {
+  // enum values
+  const types = params.type
+    .split("|")
+    .map((type) => Number.parseInt(type)) as FileType[];
+  const realFileTypes: string[] = [];
+  if (types.length === 0) {
+    forEach(allowedTypes, (type) => {
+      realFileTypes.push(type);
+    });
+  }
 
-    // enum values
-    const types = params.type.split("|").map(type => Number.parseInt(type)) as FileType[];
-    const realFileTypes: string[] = [];
-    if (types.length === 0) {
-        forEach(allowedTypes, type => {
-            realFileTypes.push(type);
-        })
+  forEach(types, (type) => {
+    switch (type) {
+      case FileType.Image:
+        forEach(allowedImageTypes, (imageType) => {
+          realFileTypes.push(imageType);
+        });
+        break;
+      case FileType.Video:
+        forEach(allowedVideoTypes, (videoType) => {
+          realFileTypes.push(videoType);
+        });
+        break;
+      case FileType.Audio:
+        forEach(allowedAudioTypes, (audioType) => {
+          realFileTypes.push(audioType);
+        });
+        break;
+      case FileType.Document:
+        forEach(allowedDocumentTypes, (documentType) => {
+          realFileTypes.push(documentType);
+        });
+        break;
     }
+  });
 
-    forEach(types, type => {
-        switch (type) {
-            case FileType.Image:
-                forEach(allowedImageTypes, imageType => {
-                    realFileTypes.push(imageType);
-                })
-                break;
-            case FileType.Video:
-                forEach(allowedVideoTypes, videoType => {
-                    realFileTypes.push(videoType);
-                })
-                break;
-            case FileType.Audio:
-                forEach(allowedAudioTypes, audioType => {
-                    realFileTypes.push(audioType);
-                })
-                break;
-            case FileType.Document:
-                forEach(allowedDocumentTypes, documentType => {
-                    realFileTypes.push(documentType);
-                })
-                break;
-        }
-    });
+  const files = await prismaDB.files.findMany({
+    where: {
+      file_type: {
+        in: realFileTypes,
+      },
+    },
+    select: {
+      id: true,
+      file_name: true,
+      file_type: true,
+      file_size: true,
+      createdAt: true,
+      updatedAt: true,
+      folder: true,
+      file_content: false,
+    },
+  });
 
-    const files = await prismaDB.files.findMany({
-        where: {
-            file_type: {
-                in: realFileTypes
-            }
-        }, select: {
-            id: true,
-            file_name: true,
-            file_type: true,
-            file_size: true,
-            createdAt: true,
-            updatedAt: true,
-            folder: true,
-            file_content: false
-        }
-    });
+  const filesWithUrl = files.map((file) => {
+    return {
+      ...file,
+      url: `${process.env.APP_HOST}/public/uploads${
+        file.folder
+      }${encodeURIComponent(file.file_name)}`,
+    };
+  });
 
-    const filesWithUrl = files.map(file => {
-        return {
-            ...file,
-            url: `${process.env.APP_HOST}/public/uploads${file.folder}${file.id}`
-        }
-    });
-
-    return getResponse(200, "Files", {files: filesWithUrl});
+  return getResponse(200, "Files", { files: filesWithUrl });
 }
--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,58 +1,65 @@
-import {NextRequest} from 'next/server'
-import {prismaDB} from "@/lib/prisma";
-import {getErrorResponse, getResponse} from "@/lib/utils";
-import {allowedDocumentTypes, allowedTypes} from "@/lib/constants/filetypes.constants";
+import { NextRequest } from "next/server";
+import { prismaDB } from "@/lib/prisma";
+import { getErrorResponse, getResponse } from "@/lib/utils";
+import {
+  allowedDocumentTypes,
+  allowedTypes,
+} from "@/lib/constants/filetypes.constants";
 import sharp from "sharp";
 
 export async function POST(request: NextRequest) {
-    const data = await request.formData();
-    console.log(data.get('file'));
-    const file: File | null = data.get('file') as unknown as File
+  const data = await request.formData();
+  console.log(data.get("file"));
+  const file: File | null = data.get("file") as unknown as File;
 
-    if (!file) {
-        return getErrorResponse(400, "Invalid file type");
-    }
+  if (!file) {
+    return getErrorResponse(400, "Invalid file type");
+  }
 
-    const bytes = await file.arrayBuffer()
-    let buffer = Buffer.from(bytes)
+  const bytes = await file.arrayBuffer();
+  let buffer = Buffer.from(bytes);
 
-    // With the file data in the buffer, you can do whatever you want with it.
-    // For this, we'll just write it to the filesystem in a new location
-    const file_size_in_bytes = buffer.length
-    let file_name = file.name;
-    let file_type = file.type;
+  // With the file data in the buffer, you can do whatever you want with it.
+  // For this, we'll just write it to the filesystem in a new location
+  const file_size_in_bytes = buffer.length;
+  let file_name = file.name;
+  let file_type = file.type;
 
-    if (allowedTypes.indexOf(file_type) === -1) {
-        return getErrorResponse(400, "Invalid file type");
-    }
+  if (allowedTypes.indexOf(file_type) === -1) {
+    return getErrorResponse(400, "Invalid file type");
+  }
 
-    const dbFile = await prismaDB.files.create({
-        data: {
-            file_name: file_name,
-            file_type: file_type,
-            file_size: file_size_in_bytes,
-            file_content: buffer
-        }
+  const dbFile = await prismaDB.files.create({
+    data: {
+      file_name: file_name,
+      file_type: file_type,
+      file_size: file_size_in_bytes,
+      file_content: buffer,
+    },
+  });
 
-    });
-
-    return getResponse(200, "File uploaded", {
-        file: {...dbFile, url: `${process.env.APP_HOST}/public/uploads/${dbFile.id}`}
-    });
+  return getResponse(200, "File uploaded", {
+    file: {
+      ...dbFile,
+      url: `${process.env.APP_HOST}/public/uploads${
+        dbFile.folder
+      }${encodeURIComponent(dbFile.file_name)}`,
+    },
+  });
 }
 
 const isImage = (file: File) => {
-    return file.type.startsWith("image/");
-}
+  return file.type.startsWith("image/");
+};
 
 const isVideo = (file: File) => {
-    return file.type.startsWith("video/");
-}
+  return file.type.startsWith("video/");
+};
 
 const isAudio = (file: File) => {
-    return file.type.startsWith("audio/");
-}
+  return file.type.startsWith("audio/");
+};
 
 const isDocument = (file: File) => {
-    return allowedDocumentTypes.indexOf(file.type) !== -1;
-}
+  return allowedDocumentTypes.indexOf(file.type) !== -1;
+};
